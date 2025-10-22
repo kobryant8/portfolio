7da4ea7c44c7263a7aab52b7fb7e3a44fa043c6a
kobryant8 <gudwns9947@gmail.com>
2025-10-22 15:40:50 +0900

    Add files via upload

--- portfolio-data.js
+++ portfolio-data.js
@@ -40,38 +40,40 @@ var portfolioItems = [
     image: 'assets/po5.png',
     url: 'https://www.youtube.com/watch?v=zDhzu2kl76M'
   },
-    {
+  {
     title: '프로젝트 6',
     description: '학원에서 팀원들과 제작한 유튜브 감성 코미디 모음 영상입니다. 무도실무관 파트를 맡았습니다.',
     image: 'assets/po6.png',
     url: 'https://www.youtube.com/watch?v=qc44fWQWUQY'
   },
-    {
+  {
     title: '프로젝트 7',
     description: '학원에서 팀원들과 제작한 유튜브 감성 snl 하이라이트 영상입니다. 콩트와 효과를 제외한 나머지 모든 코너를 맡았습니다.',
     image: 'assets/po7.png',
     url: 'https://www.youtube.com/watch?v=B3Jr8WpAPAo'
   },
-    {
+  {
     title: '프로젝트 8',
     description: '농구 리그 NBA를 대표하는 선수들의 MVP 시즌 하이라이트 영상입니다. 모션그래픽을 포함한 편집을 했습니다.',
     image: 'assets/po8.png',
     url: 'https://www.youtube.com/watch?v=Jpo9D5d9IG4'
-  },  {
+  },  
+  {
     title: '프로젝트 9',
     description: '대학 때 만든 공포 드라마 케이지의 스토리 요약 영상입니다. 각본, 음향, 편집을 담당했습니다.',
     image: 'assets/po9.png',
     url: 'https://www.youtube.com/watch?v=JDNcqOB0DKw'
-  },  {
+  },  
+  {
     title: '프로젝트 10',
     description: '픽사베이의 상업용 무료 영상과 미드저니 AI 툴을 이용하어 커피광고를 만들었습니다. 자료 확보와 가편집을 담당하였습니다.',
     image: 'assets/po10.png',
     url: 'https://www.youtube.com/watch?v=si_8xcDC9Vg'
   },
-    {
+  {
     title: '프로젝트 11',
     description: '벤슨 분의 음악 "Beautiful Things"를 기반으로 한 AI 뮤직드라마입니다. 기획과 영상 생성을 담당하였습니다.',
     image: 'assets/po11.png',
     url: 'https://www.youtube.com/watch?v=XYNLVchIT18'
-  },
+  }
 ];
